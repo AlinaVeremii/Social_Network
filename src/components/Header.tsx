0b7ccd63f6c9d6c1e590763bea1aafcf55122b9b
Alina <146032578+AlinaVeremii@users.noreply.github.com>
2023-11-26 17:02:27 +0100

    Was added components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src="https://img.freepik.com/darmowe-wektory/ptak-kolorowe-logo-wektor-gradientu_343694-1365.jpg"
+                 alt="image"/>
+        </header>
+    );
+}
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                {/*<img src="https://img.freepik.com/premium-photo/lmage-young-woman-sitting-her-laptop_948771-155.jpg"*/}
+                {/*     alt=""/>*/}
+            </div>
+            <div><img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg02gzmfr4rAWbHHFvOfMjsm02-y1Er5GDHw&usqp=CAU"
+                alt=""/></div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                    <div>post 3</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Profile
