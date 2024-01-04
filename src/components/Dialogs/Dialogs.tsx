c38043aa18435f88683b321ad8ee8a5e0bcbd9c5
Alina <146032578+AlinaVeremii@users.noreply.github.com>
2024-01-04 17:10:20 +0100

    l22.Route exact

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 type DialogsPropsType = {}
 
@@ -8,11 +9,21 @@ function Dialogs(props: any) {
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <h2>Dialogs</h2>
-                <div className={s.dialog}>Name1</div>
-                <div className={s.dialog}>Name2</div>
-                <div className={s.dialog}>Name3</div>
-                <div className={s.dialog}>Name4</div>
-                <div className={s.dialog}>Name5</div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/1'>Name1</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Name2</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>Name3</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'>Name4</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5'>Name5</NavLink>
+                </div>
             </div>
             <div className={s.messages}>
                 <h3>Messages</h3>
