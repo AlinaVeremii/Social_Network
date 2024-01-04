3d5f8e3fbd38918bfd24020af83c491e5a214bbb
Alina <146032578+AlinaVeremii@users.noreply.github.com>
2024-01-04 16:47:34 +0100

    l20.Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 
-type DialogsPropsType = {
+type DialogsPropsType = {}
 
-}
-function Dialogs(props:any) {
+function Dialogs(props: any) {
     return (
-        <div>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <h2>Dialogs</h2>
+                <div className={s.dialog}>Name1</div>
+                <div className={s.dialog}>Name2</div>
+                <div className={s.dialog}>Name3</div>
+                <div className={s.dialog}>Name4</div>
+                <div className={s.dialog}>Name5</div>
+            </div>
+            <div className={s.messages}>
+                <h3>Messages</h3>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How u r</div>
+                <div className={s.message}>yoo</div>
+
+            </div>
+        </div>
     )
 }
 
